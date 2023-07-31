2acfacf6a87a14f2c59d4fc23f0812f4e9c64465
motilal-kumar <motilal.kumar@neosoft.com>
2023-07-31 10:42:23 +0530

    Initial commit This is APIGATEWAY project.

---
+++ src/main/java/com/neosoft/gateway/ApiGateway/model/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.neosoft.gateway.ApiGateway.model;
+
+import lombok.*;
+
+import java.util.Collection;
+
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String userId;
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private long expireAt;
+
+    private Collection<String>  authorities;
+}
+
+
